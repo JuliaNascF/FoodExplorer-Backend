c6123da353375ffffadf03fd9519bcf665e16d7a
JuliaNascF <jubilete.fazenda12@gmail.com>
2023-08-09 01:33:50 -0300

    fix: joi sign up schema

--- src/routes/sessions.routes.js
+++ src/routes/sessions.routes.js
@@ -2,12 +2,12 @@ const { Router } = require("express");
 
 const SessionsController = require("../controllers/SessionsController");
 const sessionsController = new SessionsController();
-const loginSchema = require("../schemas/auth.schemas.js");
+const userSchema = require("../schemas/auth.schemas.js");
 
 const sessionsRoutes = Router();
 
 const validateLoginSchema = (req, res, next) => {
-    const { error } = loginSchema.validate(req.body);
+    const { error } = userSchema.validate(req.body);
     if (error) {
       return res.status(400).json({ error: error.message });
     }
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,14 +2,13 @@ const { Router } = require('express');
 const multer = require('multer');
 const uploadConfig = require('../configs/multer');
 const UsersController = require("../controllers/UsersController");
-const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
-const  userSchema  = require("../schemas/auth.schemas.js");
+const  loginSchema  = require("../schemas/auth.schemas.js");
 
 const usersController = new UsersController();
 const usersRoutes = Router();
 
 const validateUserSchema = (req, res, next) => {
-    const { error } = userSchema.validate(req.body);
+    const { error } = loginSchema.validate(req.body);
     if (error) {
       return res.status(400).json({ error: error.message });
     }
