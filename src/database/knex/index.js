1752b0712a9d46d6a341c70657927a1efad8af76
JuliaNascF <jubilete.fazenda12@gmail.com>
2023-08-08 23:37:09 -0300

    refactor: fix 1

--- src/database/knex/index.js
+++ src/database/knex/index.js
@@ -5,6 +5,6 @@ const dotenv=require("dotenv");
 dotenv.config();
 
 const environment = process.env.NODE_ENV || 'development';
-const connection = knex(knexfile[environment]);
+const connection = knex(knexfile.development);
 
 module.exports= connection;
