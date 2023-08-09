d6fbb805ae0e903da5afcbce3dc6723eedbda5c5
JuliaNascF <jubilete.fazenda12@gmail.com>
2023-08-08 23:40:56 -0300

    fix: port import of dotenv

--- src/server/server.js
+++ src/server/server.js
@@ -35,7 +35,7 @@ function connectServer(){
   });
     
     
-    const PORT = process.env.PORT || 4000
+    const PORT = 4000
     app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
    
   };
