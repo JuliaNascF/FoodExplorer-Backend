3aee18cea83d75012054d940b0768dfc30267492
JuliaNascF <jubilete.fazenda12@gmail.com>
2023-08-08 23:29:08 -0300

    refactor: add knexjs file

---
+++ knexfile.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: 'ejvllvtg',
+      user: 'ejvllvtg',
+      password: 'IufuDDWLzmVMtc0kxXj2OuqsfA_2QQ8z'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: path.resolve(__dirname, 'src', 'database', 'knex', 'migrations')
+    }
+  },
+
+  test: {
+    client: 'sqlite3',
+    connection: {
+      filename: path.resolve(__dirname, '__tests__' , 'test.db') 
+    },
+    useNullAsDefault: true,
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: path.resolve(__dirname, 'src', 'database', 'knex', 'migrations')
+    }
+  }
+ 
+};
